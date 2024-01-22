9c144d9e934e1942f23d182b04eca086e3359bd7
Mark Parrish <mark@pipcommunications.com>
2024-01-22 08:05:04 -0800

    initial physical_id for places index

--- src/utils/search.py
+++ src/utils/search.py
@@ -1,5 +1,6 @@
 from common_functions.search import MyElasticsearch
 from utils.config import ES_HOST, ES_USERNAME, ES_PASSWORD, CA_CERT_PATH
+import pandas as pd
 
 # Example usage:
 es_host = ES_HOST
@@ -23,17 +24,17 @@ class MyExtendedElasticsearch(MyElasticsearch):
         query = {
             "query": {
                 "bool": {
-                "must": [
-                    {
-                    "simple_query_string": {
-                        "query": address_query,
+                    "must": [
+                        {
+                            "simple_query_string": {
+                                "query": address_query,
 
-                        "fields": ["PropertyAddress*"],
-                        "default_operator": "AND",
-                        "analyzer": "standard"
-                    }
-                    }
-                ]
+                                "fields": ["PropertyAddress*"],
+                                "default_operator": "AND",
+                                "analyzer": "standard"
+                            }
+                        }
+                    ]
                 }
             },
             "collapse": {
@@ -42,5 +43,110 @@ class MyExtendedElasticsearch(MyElasticsearch):
         }
         response = self.client.options(basic_auth=(self.username, self.password)).search(index=index_name, body=query)
         return response
+
+    def exact_match_address(self, index_name, house_number, street_name, zip_code):
+        """
+        Search for an address in the Elasticsearch index.
+
+        Args:
+            index_name (str): The name of the Elasticsearch index to search.
+            address_query (str): The address query to search for.
+
+        Returns:
+            dict: The search results as a dictionary.
+        """
+        # Create an Elasticsearch query DSL for address search
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "term": {
+                                "PropertyAddressHouseNumber.raw": house_number
+                            }
+                        },
+                        {
+                            "term": {
+                                "PropertyAddressStreetName.raw": street_name
+                            }
+                        },
+                        {
+                            "term": {
+                                "PropertyAddressZIP.raw": zip_code
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+        response = self.client.options(basic_auth=(self.username, self.password)).search(index=index_name, body=query)
+        return response
+
+def process_search_results(search_results):
+    hits_count = len(search_results['hits']['hits'])
+
+    if hits_count == 1:
+        hit = search_results['hits']['hits'][0]['_source']
+        return handle_single_result(hit)
+
+    if hits_count > 1:
+        return handle_multiple_results()
+
+    return handle_no_results()
+
+def handle_single_result(hit):
+    return pd.Series({
+        "results": "Success",
+        "phsyical_id": hit.get("[ATTOM ID]"),
+        "PropertyAddressFull": hit.get("PropertyAddressFull"),
+        "PropertyAddressHouseNumber": hit.get("PropertyAddressHouseNumber"),
+        "PropertyAddressStreetDirection": hit.get("PropertyAddressStreetDirection"),
+        "PropertyAddressStreetName": hit.get("PropertyAddressStreetName"),
+        "PropertyAddressStreetSuffix": hit.get("PropertyAddressStreetSuffix"),
+        "PropertyAddressCity": hit.get("PropertyAddressCity"),
+        "PropertyAddressState": hit.get("PropertyAddressState"),
+        "PropertyAddressZIP": hit.get("PropertyAddressZIP"),
+        "PropertyAddressZIP4": hit.get("PropertyAddressZIP4"),
+        "PropertyAddressCRRT": hit.get("PropertyAddressCRRT"),
+        "PropertyLatitude": hit.get("PropertyLatitude"),
+        "PropertyLongitude": hit.get("PropertyLongitude")
+    })
+
+def handle_multiple_results():
+    return pd.Series({
+        "results": "Too Many Results",
+        "phsyical_id": "",
+        "PropertyAddressFull": "",
+        "PropertyAddressHouseNumber": "",
+        "PropertyAddressStreetDirection": "",
+        "PropertyAddressStreetName": "",
+        "PropertyAddressStreetSuffix": "",
+        "PropertyAddressCity": "",
+        "PropertyAddressState": "",
+        "PropertyAddressZIP": "",
+        "PropertyAddressZIP4": "",
+        "PropertyAddressCRRT": "",
+        "PropertyLatitude": "",
+        "PropertyLongitude": ""
+    })
+
+def handle_no_results():
+    return pd.Series({
+        "results": "No Results",
+        "phsyical_id": "",
+        "PropertyAddressFull": "",
+        "PropertyAddressHouseNumber": "",
+        "PropertyAddressStreetDirection": "",
+        "PropertyAddressStreetName": "",
+        "PropertyAddressStreetSuffix": "",
+        "PropertyAddressCity": "",
+        "PropertyAddressState": "",
+        "PropertyAddressZIP": "",
+        "PropertyAddressZIP4": "",
+        "PropertyAddressCRRT": "",
+        "PropertyLatitude": "",
+        "PropertyLongitude": ""
+    })
+
 
 search_client = MyExtendedElasticsearch(es_host, ca_cert_path, es_username, es_password)
--- src/voterfile/voters_transform.py
+++ src/voterfile/voters_transform.py
@@ -17,7 +17,7 @@ from pandarallel import pandarallel
 from common_functions.common import get_traceback, get_timing
 from common_functions.physical_address import standardize_address
 from common_functions.file_operations import read_extract, write_load
-from utils.search import search_client
+from utils.search import search_client, process_search_results
 from utils.arg_parser import get_date_and_sample
 
 from voterfile_data_contract import DATA_CONTRACT, STATE, TABLENAME
@@ -33,67 +33,16 @@ def truncate_words(address, max_length=10):
     # Join the truncated words back into a string
     return ' '.join(truncated_words)
 
-# Define a function to apply 'standardize_address' to each row
 def search_for_address(address):
-    index_name = "attom"
-
-    # address = truncate_words(address)
-
+    index_name = "places"
     search_results = search_client.search_address(index_name, address)
+    return process_search_results(search_results)
 
-    if len(search_results['hits']['hits']) == 1:
-        hit = search_results['hits']['hits'][0]['_source']
-        return pd.Series({
-            "results": "Success",
-            "phsyical_id": hit.get("[ATTOM ID]"),
-            "PropertyAddressFull": hit.get("PropertyAddressFull"),
-            "PropertyAddressHouseNumber": hit.get("PropertyAddressHouseNumber"),
-            "PropertyAddressStreetDirection": hit.get("PropertyAddressStreetDirection"),
-            "PropertyAddressStreetName": hit.get("PropertyAddressStreetName"),
-            "PropertyAddressStreetSuffix": hit.get("PropertyAddressStreetSuffix"),
-            "PropertyAddressCity": hit.get("PropertyAddressCity"),
-            "PropertyAddressState": hit.get("PropertyAddressState"),
-            "PropertyAddressZIP": hit.get("PropertyAddressZIP"),
-            "PropertyAddressZIP4": hit.get("PropertyAddressZIP4"),
-            "PropertyAddressCRRT": hit.get("PropertyAddressCRRT"),
-            "PropertyLatitude": hit.get("PropertyLatitude"),
-            "PropertyLongitude": hit.get("PropertyLongitude")
-        })
+def exact_match_address(house_number, street_name, zip_code):
+    index_name = "places"
+    search_results = search_client.exact_match_address(index_name, house_number, street_name, zip_code)
+    return process_search_results(search_results)
 
-    if len(search_results['hits']['hits']) > 1:
-        return pd.Series({
-            "results": "Too Many Results",
-            "phsyical_id": "",
-            "PropertyAddressFull": "",
-            "PropertyAddressHouseNumber": "",
-            "PropertyAddressStreetDirection": "",
-            "PropertyAddressStreetName": "",
-            "PropertyAddressStreetSuffix": "",
-            "PropertyAddressCity": "",
-            "PropertyAddressState": "",
-            "PropertyAddressZIP": "",
-            "PropertyAddressZIP4": "",
-            "PropertyAddressCRRT": "",
-            "PropertyLatitude": "",
-            "PropertyLongitude": ""
-        })
-
-    return pd.Series({
-        "results": "No Results",
-        "phsyical_id": "",
-        "PropertyAddressFull": "",
-        "PropertyAddressHouseNumber": "",
-        "PropertyAddressStreetDirection": "",
-        "PropertyAddressStreetName": "",
-        "PropertyAddressStreetSuffix": "",
-        "PropertyAddressCity": "",
-        "PropertyAddressState": "",
-        "PropertyAddressZIP": "",
-        "PropertyAddressZIP4": "",
-        "PropertyAddressCRRT": "",
-        "PropertyLatitude": "",
-        "PropertyLongitude": ""
-    })
 
 def _transform(df) -> pd.DataFrame:
     """
@@ -103,26 +52,84 @@ def _transform(df) -> pd.DataFrame:
     Write unmatched addresses to file adding to places table and for geocoding (only physical)
     Write processed voters to file having the right columns
     """
+
+    # fill na with empty string
+    df = df.fillna('')
     print("...standardizing physical address")
     # find address
     df['find_address'] = df['physical_house_number'].astype(str) + ' ' + df['physical_street_name'].astype(str) + ' ' + df['physical_zip_code'].astype(str)
     address_df = df['find_address'].parallel_apply(search_for_address)
     df = df.join(address_df)
-    # Define conditions
 
-    # df['flattened_response'] = df['found_address'].parallel_apply(flatten_response)
+    # Add Confidetial to Results where the address is confidential
+    # Confidential address are not geocoded and have a physical_id equal to the precinct_link
+    df.loc[df['confidential'] == 'Confidential', 'results'] = 'Confidential'
+    df.loc[df['confidential'] == 'Confidential', 'physical_id'] = df['precinct_link']
 
-    # # Convert the flattened responses to a DataFrame
-    # df_flattened = pd.json_normalize(df['flattened_response'])
+    # frist pass on too many results
+    # see if there is an exact match on the house number, street and zip code
+    mask = (df['results'] == "Too Many Results")
+    df[["results",
+        "phsyical_id",
+        "PropertyAddressFull",
+        "PropertyAddressHouseNumber",
+        "PropertyAddressStreetDirection",
+        "PropertyAddressStreetName",
+        "PropertyAddressStreetSuffix",
+        "PropertyAddressCity",
+        "PropertyAddressState",
+        "PropertyAddressZIP",
+        "PropertyAddressZIP4",
+        "PropertyAddressCRRT",
+        "PropertyLatitude",
+        "PropertyLongitude"]] = df.parallel_apply(lambda x: exact_match_address(x['physical_house_number'], x['physical_street_name'], x['physical_zip_code']) if mask[x.name] else
+            (x["results"],
+            x["phsyical_id"],
+            x["PropertyAddressFull"],
+            x["PropertyAddressHouseNumber"],
+            x["PropertyAddressStreetDirection"],
+            x["PropertyAddressStreetName"],
+            x["PropertyAddressStreetSuffix"],
+            x["PropertyAddressCity"],
+            x["PropertyAddressState"],
+            x["PropertyAddressZIP"],
+            x["PropertyAddressZIP4"],
+            x["PropertyAddressCRRT"],
+            x["PropertyLatitude"],
+            x["PropertyLongitude"]), axis=1, result_type="expand")
 
-    # # Concatenate the new DataFrame with the original DataFrame
-    # df = pd.concat([df, df_flattened], axis=1)
-
-    # # Create a boolean mask for rows with "Too Many Results" and a non-empty 'physical_unit_type'
-    # mask = (df['found_address'] == "Too Many Results") & df['physical_unit_type'].notna()
-    # # Use the mask to assign values in the new column
-    # df['apartment_flag'] = "Not Apartment"  # Default value
-    # df.loc[mask, 'apartment_flag'] = "Possible Apartment"
+    # deal with addresses that have a unit number (Condos, Apartments, etc)
+    # need to reapply the mask becuase the df has changed
+    mask = (df['results'] == "Too Many Results")
+    df['find_address'] = df['physical_house_number'].astype(str) + ' ' + df['physical_street_name'].astype(str) + ' ' + df['physical_zip_code'].astype(str) + ' ' + df['physical_unit_number'].astype(str)
+    df[["results",
+        "phsyical_id",
+        "PropertyAddressFull",
+        "PropertyAddressHouseNumber",
+        "PropertyAddressStreetDirection",
+        "PropertyAddressStreetName",
+        "PropertyAddressStreetSuffix",
+        "PropertyAddressCity",
+        "PropertyAddressState",
+        "PropertyAddressZIP",
+        "PropertyAddressZIP4",
+        "PropertyAddressCRRT",
+        "PropertyLatitude",
+        "PropertyLongitude"]] = df.parallel_apply(lambda x: search_for_address(x['find_address']) if mask[x.name] else
+            (x["results"],
+            x["phsyical_id"],
+            x["PropertyAddressFull"],
+            x["PropertyAddressHouseNumber"],
+            x["PropertyAddressStreetDirection"],
+            x["PropertyAddressStreetName"],
+            x["PropertyAddressStreetSuffix"],
+            x["PropertyAddressCity"],
+            x["PropertyAddressState"],
+            x["PropertyAddressZIP"],
+            x["PropertyAddressZIP4"],
+            x["PropertyAddressCRRT"],
+            x["PropertyLatitude"],
+            x["PropertyLongitude"]), axis=1, result_type="expand")
 
     # df = df.drop('find_address', axis=1)
     return df.reset_index(drop=True)
@@ -144,7 +151,7 @@ def main():
         df = df.sample(n=sample)
     df = _transform(df)
     df = write_load(df, final_data_path)
-
+    print('')
     print(f"File Processed {len(df)} records")
     # for col in df.columns:
     #     print(col)
